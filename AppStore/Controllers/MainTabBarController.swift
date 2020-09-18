f78bb44dbd6bb7e05066a8289c737cc4d0628fa7
Niko Kim <the.fear.dev@gmail.com>
2020-09-18 10:08:24 +0600

    Tab Bar added, with 3 tabs

---
+++ AppStore/Controllers/MainTabBarController.swift
@@ -0,0 +1,34 @@
+//
+//  MainTabBarController.swift
+//  AppStore
+//
+//  Created by Niko Kim on 18.09.2020.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    viewControllers = [
+      createNavigationController(viewController: UIViewController(), title: "Apps", image: "apps"),
+      createNavigationController(viewController: UIViewController(), title: "Search", image: "search"),
+      createNavigationController(viewController: UIViewController(), title: "Today", image: "today_icon")
+    ]
+  }
+  
+  fileprivate func createNavigationController(viewController: UIViewController, title: String, image: String) -> UINavigationController {
+    
+    viewController.view.backgroundColor = .systemBlue
+    viewController.navigationItem.title = title
+    
+    let navigationController = UINavigationController(rootViewController: viewController)
+    navigationController.tabBarItem.title = title
+    navigationController.tabBarItem.image = UIImage(named: image)
+    navigationController.navigationBar.prefersLargeTitles = true
+    return navigationController
+    
+  }
+}
